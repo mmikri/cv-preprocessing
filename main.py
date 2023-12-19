d9cd71a4b38f99df96079874e3581eeecc0209ca
mmikri <mirine.kr.97@gmail.com>
2023-12-19 20:24:26 +0900

    20231219 add Initial UI Development

--- main.py
+++ main.py
@@ -1,63 +1,52 @@
 # main.py
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.metrics import dp
 import os
-import argparse
-from dotenv import load_dotenv
-from app import __version__
-from utils.__env import *
-from utils.conf import load_config
-from utils.file import create_output_directory, get_supported_files
-from utils.preprocess import preprocess_media
 
-def create_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(description="PreProcessing")
-    parser.add_argument(
-        "-t",
-        "--type",
-        type=str,
-        default="image",
-        choices=["image", "i", "video", "v"],
-        help="Specify the data type: 'image(i)' or 'video(v)' (default: 'image')"
-    )
-    parser.add_argument(
-        "-p",
-        "--path",
-        type=str,
-        default="./assets",
-        help="Path to images or video files (default: './assets')"
-    )
-    parser.add_argument(
-        "-c",
-        "--config",
-        default="./config.yaml",
-        type=str,
-        help="Path to the configuration file (default: './config.yaml')"
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        default="./result",
-        type=str,
-        help="Path to the output directory (default: './result')"
-    )
-    return parser
+class TestApp(App):
+    def build(self):
+        Window.size = (1536, 864)
+        return MyBoxLayout()
 
-def main(args: argparse.Namespace) -> None:
-    print(f"==================================")
-    print(f"The app version is {__version__}")
-    print(f"==================================")
+class MyBoxLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MyBoxLayout, self).__init__(**kwargs)
+        self.bind(on_parent=self.on_parent)
 
-    config_data = load_config(args.config)
-    output_directory = args.output
-    create_output_directory(output_directory)
-    data_type = args.type
-    input_path = args.path
+    def on_parent(self, instance, value):
+        if value:
+            left_box = self.ids.left_box
+            if left_box:
+                left_box.bind(on_size=self.update_size_hint_x)
 
-    supported_files = get_supported_files(input_path, data_type)
+    def update_size_hint_x(self, instance, value):
+        left_box = self.ids.left_box
+        if left_box:
+            left_box.size_hint_x = instance.width / self.width
 
-    if supported_files:
-        media_settings = config_data.get("image_settings", {}) if data_type in ["image", "i"] else config_data.get("video_settings", {})
-        preprocess_media(supported_files, output_directory, media_settings)
+class LeftBox(BoxLayout):
+    pass
 
-if __name__ == "__main__":
-    command_args = create_parser().parse_args()
-    main(command_args)
+class CenterBox(BoxLayout):
+    pass
+
+class RightBox(BoxLayout):
+    def update_label_text(self, button_text):
+        self.ids.right_label.text = f"{button_text}"
+
+class MyAppButton(Button):
+    def __init__(self, **kwargs):
+        super(MyAppButton, self).__init__(**kwargs)
+        self.bind(on_release=self.on_button_release)
+
+    def on_button_release(self, instance):
+        app = App.get_running_app()
+        app.root.ids.right_box.update_label_text(self.text)
+
+if __name__ == '__main__':
+    TestApp().run()
